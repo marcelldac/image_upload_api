fbae05266323a5cee287b6affa722c1b53e0c825
marcelldac <dacmarcell@proton.me>
2023-08-29 15:51:23 -0300

    feat(picController): send image

---
+++ controllers/pictureController.js
@@ -0,0 +1,18 @@
+const Picture = require('../models/Picture');
+
+exports.create = async (req, res) => {
+    try{
+        const {name} = req.body;
+        const file = req.file;
+        const picture = new Picture({
+            name,
+            src: file.path
+        });
+
+        await picture.save();
+
+        res.json({picture, msg: "Imagem salva com sucesso!"})
+    }catch(error){
+        res.status(500).json({mensagem: "Erro ao salvar imagem."})
+    }
+};
