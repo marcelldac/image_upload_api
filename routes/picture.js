67e0560e8b410042455809cfb4dc13bf84edcc67
marcelldac <dacmarcell@proton.me>
2023-08-29 15:58:22 -0300

    feat(picture): add method to show all images

--- routes/picture.js
+++ routes/picture.js
@@ -5,5 +5,6 @@ const upload = require('../config/multer');
 const PictureController = require('../controllers/pictureController');
 
 router.post('/', upload.single('file'), PictureController.create);
+router.get('/', PictureController.findAll);
 
 module.exports = router;
