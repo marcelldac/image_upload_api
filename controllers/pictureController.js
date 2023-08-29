67e24cd17820149cdbb60d628db36d27e8e89151
marcelldac <dacmarcell@proton.me>
2023-08-29 16:53:27 -0300

    feat: add remove image method

--- controllers/pictureController.js
+++ controllers/pictureController.js
@@ -1,4 +1,5 @@
 const Picture = require('../models/Picture');
+const fs = require('fs');
 
 exports.create = async (req, res) => {
     try{
@@ -23,5 +24,19 @@ exports.findAll = async (req, res) => {
         res.json(pictures);
     }catch(error){
         res.status(500).json({message: "Erro ao buscar imagens."});
+    }
+}
+
+exports.remove = async (req, res) => {
+    try{
+        const picture = await Picture.findByIdAndDelete(req.params.id);
+        if(!picture){
+            return res.status(404).json({message: "Imagem não encontrada."});
+        }
+        fs.unlinkSync(picture.src);
+        res.json({message: "Imagem removida com sucesso!"});
+    }catch(error){
+        res.status(500).json({message: "Erro ao excluir imagem."});
+        console.log(error);
     }
 }
--- routes/picture.js
+++ routes/picture.js
@@ -6,5 +6,6 @@ const PictureController = require('../controllers/pictureController');
 
 router.post('/', upload.single('file'), PictureController.create);
 router.get('/', PictureController.findAll);
+router.delete('/:id', PictureController.remove);
 
 module.exports = router;
