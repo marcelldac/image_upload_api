66a3e579fbcb8bdb5e52bd9a98c64d2fe3f5a362
marcelldac <dacmarcell@proton.me>
2023-08-29 15:53:28 -0300

    feat(picture): create post req to send image

---
+++ routes/picture.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+const upload = require('../config/multer');
+const PictureController = require('../controllers/pictureController');
+
+router.post('/', upload.single('file'), PictureController.create);
+
+module.exports = router;
