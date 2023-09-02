863dbabe4df34bad9269e18972b13267fd765500
marcelldac <dacmarcell@proton.me>
2023-09-01 22:16:57 -0300

    fix(db): new database connection

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
 mongoose.set("strictQuery", true);
 
 async function main(){
-    await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.qv7w4dv.mongodb.net/`)
+    await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.qbfmten.mongodb.net/?retryWrites=true&w=majority`)
     log("Conectado com sucesso!")
 }
 
-main().catch((err) => console.log(err))
+main().catch((err) => log(err))
 
 module.exports = main;
