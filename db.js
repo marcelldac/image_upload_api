c95ae1ecac7711ec98051cce653cd73f096ca1c8
marcelldac <devmarcell@protonmail.com>
2024-06-09 15:15:01 -0300

    feat: Update database connection logic

--- db.js
+++ db.js
@@ -1,14 +1,21 @@
-require('dotenv').config();
-const log = console.log;
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 mongoose.set("strictQuery", true);
 
-async function main(){
-    await mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.qbfmten.mongodb.net/?retryWrites=true&w=majority`)
-    log("Conectado com sucesso!")
+async function main() {
+  try {
+    const databaseUrl = process.env.DATABASE_URI;
+    if (!databaseUrl) {
+      throw new Error("Variável de ambiente DATABASE_URI não foi definida");
+    }
+    await mongoose.connect(databaseUrl);
+    console.log("Conectado ao banco de dados");
+  } catch (err) {
+    console.error("Erro ao conectar ao banco de dados: \n", err);
+  }
 }
 
-main().catch((err) => log(err))
+main();
 
 module.exports = main;
