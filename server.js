05909a1768509d265fd1f5606226a45784b721f4
marcelldac <devmarcell@protonmail.com>
2024-06-09 15:15:06 -0300

    chore: Update server.js to use ES6 syntax and improve code formatting

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
-require('./db');
-const log = console.log;
-const express = require('express');
+require("dotenv").config();
+require("./db");
+
+const express = require("express");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-const pictureRouter = require('./routes/picture');
+const pictureRouter = require("./routes/picture");
 
-app.use('/pictures', pictureRouter)
+app.use("/pictures", pictureRouter);
 
 app.listen(port, () => {
-    log(`Servidor rodando na porta ${port}`)
-})
+  console.log(`Servidor rodando na porta ${port}`);
+});
